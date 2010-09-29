e7bab1444f45701a8c65714b8aac273eaf4efd3b
Ronny Pfannschmidt <Ronny.Pfannschmidt@gmx.de>
2010-09-30 01:05:09 +0200

    sanity checks on continuations and comments

--- test_iniconfig.py
+++ test_iniconfig.py
@@ -30,10 +30,29 @@ parsings = {
         'names =\n Alice\n Bob',
         [(1, None, 'names', 'Alice\nBob')]
     ),
+    'value with aligned continuation': (
+        'names = Alice\n'
+        '        Bob',
+        [(1, None, 'names', 'Alice\nBob')]
+    ),
     'blank line':(
         '[section]\n\nvalue=1',
         [(1, 'section', None, None), (3, 'section', 'value', '1')]
     ),
+    'comment': (
+        '# comment',
+        []
+    ),
+    'comment on value': (
+        'value = 1 # comment',
+        [(1, None, 'value', '1')]
+    ),
+
+    'comment on section': (
+        '[section] #comment',
+        [(1, 'section', None, None)]
+    ),
+
 
 
 }
@@ -51,4 +70,15 @@ def pytest_generate_tests(metafunc):
 def test_parse(input, expected):
     parsed = parse(input)
     assert parsed == expected
+
+
+def test_continuation_needs_perceeding_name():
+    with py.test.raises(ValueError) as excinfo:
+        parse(' Foo')
+    assert 'line 1' in excinfo.value.args[0]
+
+def test_continuation_cant_be_after_section():
+    with py.test.raises(ValueError) as excinfo:
+        parse('[section]\n Foo')
+    assert 'line 2' in excinfo.value.args[0]
 
