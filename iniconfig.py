df14648cadfaa9ce6043722c25f7e123fc8fd0a4
holger krekel <holger@merlinux.eu>
2010-09-30 10:53:11 +0200

    more meat

---
+++ iniconfig.py
@@ -0,0 +1,103 @@
+
+__version__ = "1.0"
+
+def parseline(line, lineno):
+    
+    # comments
+    #XXX: should we support escaping #
+    line = line.split('#')[0].rstrip()
+
+    # blank lines
+    if not line:
+        return None, None
+    # section
+    if line[0] == '[' and line[-1] == ']':
+        return line[1:-1], None
+    # value
+    elif not line[0].isspace() and '=' in line:
+        name, value = line.split('=', 2)
+        return name.strip(), value.strip()
+    # continuation
+    elif line[0].isspace():
+        return None, line.strip()
+    raise ValueError('unexpected line %s %r'%(lineno, line))
+
+
+def _parse(data):
+    result = []
+    section = None
+    for line_index, line in enumerate(data.splitlines(True)):
+        lineno = line_index+1
+
+        name, data = parseline(line, lineno)
+        # new value
+        if name is not None and data is not None:
+            result.append((lineno, section, name, data))
+        # new section
+        elif name is not None and data is None:
+            if not name:
+                raise ValueError('empty section name in line%s'%lineno)
+            section = name
+            result.append((lineno, section, None, None))
+        # continuation
+        elif name is None and data is not None:
+            if not result:
+                raise ValueError(
+                    'unexpected value continuation in line %s'%lineno)
+
+            last = result.pop()
+            last_name, last_data = last[-2:]
+            if last_name is None:
+                raise ValueError(
+                    'unexpected value continuation in line %s'%lineno)
+
+            if last_data:
+                data = '%s\n%s' % (last_data, data)
+            result.append(last[:-1] + (data,))
+    return result
+
+
+class IniConfig(object):
+    def __init__(self, path=None, fp=None, data=None):
+        if path:
+            fp = open(path)
+        if fp:
+            data = fp.read()
+        tokens = _parse(data)
+        if tokens[0][1] is None:
+            raise ValueError('expected section in line %s, got name %r'%(
+                tokens[0][0],
+                tokens[0][2],
+            ))
+        self._initialize(tokens)
+
+    def _initialize(self, tokens):
+        self._sources = {}
+        self.sections = {}
+
+        for line, section, name, value in tokens:
+            self._sources[section, name] = line
+            if name is None:
+                if section in self.sections:
+                    raise ValueError(
+                        'duplicate section %r in line %s'%(section, line)
+                    )
+                self.sections[section] = {}
+            else:
+                if name in self.sections[section]:
+                    raise ValueError(
+                        'duplicate value %r in line %s'%(name, line)
+                    )
+                self.sections[section][name] = value
+
+    def lineof(self, section, name=None):
+        return self._sources.get((section, name))
+
+    def get(self, section, name, convert=str, default=None):
+        try:
+            return convert(self.sections[section][name])
+        except KeyError:
+            return default
+
+
+
---
+++ test_iniconfig.py
@@ -0,0 +1,146 @@
+import py
+from iniconfig import _parse as parse, IniConfig
+
+
+check_tokens = {
+    'section': (
+        '[section]',
+        [(1, 'section', None, None)]
+    ),
+    'value': (
+        'value = 1',
+        [(1, None, 'value', '1')]
+    ),
+    'value in section': (
+        '[section]\nvalue=1',
+        [(1, 'section', None, None), (2, 'section', 'value', '1')]
+    ),
+    'value with continuation': (
+        'names =\n Alice\n Bob',
+        [(1, None, 'names', 'Alice\nBob')]
+    ),
+    'value with aligned continuation': (
+        'names = Alice\n'
+        '        Bob',
+        [(1, None, 'names', 'Alice\nBob')]
+    ),
+    'blank line':(
+        '[section]\n\nvalue=1',
+        [(1, 'section', None, None), (3, 'section', 'value', '1')]
+    ),
+    'comment': (
+        '# comment',
+        []
+    ),
+    'comment on value': (
+        'value = 1 # comment',
+        [(1, None, 'value', '1')]
+    ),
+
+    'comment on section': (
+        '[section] #comment',
+        [(1, 'section', None, None)]
+    ),
+
+}
+
+
+weird_lines = [
+    '!!',
+    '[uhm',
+    'comeon]',
+    '[uhm =',
+    'comeon] =',
+]
+
+def pytest_generate_tests(metafunc):
+    if 'input' in metafunc.funcargnames:
+        for name, (input, expected) in check_tokens.items():
+            metafunc.addcall(id=name, funcargs={
+                'input': input,
+                'expected': expected,
+            })
+    elif hasattr(metafunc.function, 'multi'):
+        kwargs = metafunc.function.multi.kwargs
+        names, values = zip(*kwargs.items())
+        from itertools import product
+        values = product(*values)
+        for p in values:
+            metafunc.addcall(funcargs=dict(zip(names, p)))
+
+
+def test_tokenize(input, expected):
+    parsed = parse(input)
+    assert parsed == expected
+
+
+def test_continuation_needs_perceeding_token():
+    with py.test.raises(ValueError) as excinfo:
+        parse(' Foo')
+    assert 'line 1' in excinfo.value.args[0]
+
+def test_continuation_cant_be_after_section():
+    with py.test.raises(ValueError) as excinfo:
+        parse('[section]\n Foo')
+    assert 'line 2' in excinfo.value.args[0]
+
+def test_section_cant_be_empty():
+    with py.test.raises(ValueError) as excinfo:
+        parse('[]')
+
+
+@py.test.mark.multi(line=weird_lines)
+def test_error_on_weird_lines(line):
+    with py.test.raises(ValueError) as excinfo:
+        parse('!!')
+
+
+
+def test_iniconfig_from_file(tmpdir):
+    path = tmpdir/'test.txt'
+    path.write('[metadata]\nname=1')
+
+    config = IniConfig(path=str(path))
+    config2 = IniConfig(fp=path) # abuse py.path.local.read
+    config3 = IniConfig(data=path.read())
+
+def test_iniconfig_section_first(tmpdir):
+    with py.test.raises(ValueError) as excinfo:
+        IniConfig(data='name=1')
+    assert excinfo.value.args[0] == "expected section in line 1, got name 'name'"
+
+def test_iniconig_section_duplicate_fails():
+    with py.test.raises(ValueError) as excinfo:
+        IniConfig(data='[section]\n[section]')
+    assert 'duplicate section' in excinfo.value.args[0]
+
+def test_iniconfig_duplicate_key_fails():
+    with py.test.raises(ValueError) as excinfo:
+        IniConfig(data='[section]\nname = Alice\nname = bob')
+
+    assert 'duplicate value' in excinfo.value.args[0]
+
+def test_iniconfig_lineof():
+    config = IniConfig(data=
+        '[section]\n'
+        'value = 1\n'
+        '[section2]\n'
+        '# comment\n'
+        'value =2'
+    )
+
+    assert config.lineof('missing') is None
+    assert config.lineof('section') == 1
+    assert config.lineof('section2') == 3
+    assert config.lineof('section', 'value') == 2
+    assert config.lineof('section2','value') == 5
+
+def test_iniconfig_get_convert():
+    config= IniConfig(data='[section]\nint = 1\nfloat = 1.1')
+    assert config.get('section', 'int') == '1'
+    assert config.get('section', 'int', convert=int) == 1
+
+def test_iniconfig_get_missing():
+    config= IniConfig(data='[section]\nint = 1\nfloat = 1.1')
+    assert config.get('section', 'missing', default=1) == 1
+    assert config.get('section', 'missing') is None
