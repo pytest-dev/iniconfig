d7387a9fe568dbe6710f72df1a0162ac560463ec
Ronny Pfannschmidt <opensource@ronnypfannschmidt.de>
2020-10-16 20:12:24 +0200

    update tox/setuptools usage patterns and go python3

--- setup.py
+++ setup.py
@@ -10,14 +10,11 @@ from setuptools import setup
 
 
 def main():
-    with open('README.txt') as fp:
-        readme = fp.read()
     setup(
-        name='iniconfig',
         packages=['iniconfig'],
         package_dir={'': 'src'},
         description='iniconfig: brain-dead simple config-ini parsing',
-        long_description=readme,
+        
         use_scm_version=True,
         url='http://github.com/RonnyPfannschmidt/iniconfig',
         license='MIT License',
@@ -25,19 +22,6 @@ def main():
         author='Ronny Pfannschmidt, Holger Krekel',
         author_email=(
             'opensource@ronnypfannschmidt.de, holger.krekel@gmail.com'),
-        classifiers=[
-            'Development Status :: 4 - Beta',
-            'Intended Audience :: Developers',
-            'License :: OSI Approved :: MIT License',
-            'Operating System :: POSIX',
-            'Operating System :: Microsoft :: Windows',
-            'Operating System :: MacOS :: MacOS X',
-            'Topic :: Software Development :: Libraries',
-            'Topic :: Utilities',
-            'Programming Language :: Python',
-            'Programming Language :: Python :: 2',
-            'Programming Language :: Python :: 3',
-        ],
         include_package_data=True,
         zip_safe=False,
     )
