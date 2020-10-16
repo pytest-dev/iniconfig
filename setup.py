4686f71658feacdd8ad07398d1c550a10d098df1
Ronny Pfannschmidt <opensource@ronnypfannschmidt.de>
2020-10-16 20:22:27 +0200

    setup-py-upgrade

--- setup.py
+++ setup.py
@@ -1,24 +1,3 @@
-"""
-iniconfig: brain-dead simple config-ini parsing.
-
-compatible CPython 2.3 through to CPython 3.2, Jython, PyPy
-
-(c) 2010 Ronny Pfannschmidt, Holger Krekel
-"""
-
 from setuptools import setup
 
-
-def main():
-    setup(
-        packages=["iniconfig"],
-        package_dir={"": "src"},
-        use_scm_version=True,
-        url="http://github.com/RonnyPfannschmidt/iniconfig",
-        include_package_data=True,
-        zip_safe=False,
-    )
-
-
-if __name__ == "__main__":
-    main()
+setup(use_scm_version=True)
